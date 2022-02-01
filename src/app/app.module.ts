736286cc185927e57c86bcfd68b8a7c0076fd7d0
Manuel Mayo <71531146+MANUEL12-pixel@users.noreply.github.com>
2022-02-01 11:10:38 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/Navbar/navbar.component';
+import { NoticesComponent } from './components/Body/notices.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavbarComponent, NoticesComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
